04963ed03dfdaa0c6376af0b4ac4ebcf44901ade
Forpee <khalilhassam3@gmail.com>
2022-05-12 14:44:47 +0200

    have to

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,10 @@
 varying vec2 vUv;
 
 void main()
-{
+{//positiosn
     // vec4 mvPosition=modelViewMatrix*vec4(position,1.);
     // gl_PointSize=100.*(1./-mvPosition.z);
-
+    
     gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1.);
     
     vUv=uv;
